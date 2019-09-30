05169fee63955719cdc1c9d1f2e54b39c08d2bfe
Juniawan Sukrama <kotakorange@gmail.com>
2019-09-30 23:03:37 +0800

    Update logger to async

--- libs/logger.js
+++ libs/logger.js
@@ -1,10 +1,10 @@
 const axios = require("axios");
 
 module.exports = {
-  error: (scene, title, message, filePath, lineNumber, data) => {
+  error: async (scene, title, message, filePath, lineNumber, data) => {
     axios.post(process.env.URL_SERVICE_LOG+"/v1/error", {scene, title, message, filePath, lineNumber, data}).then(res => console.log).catch(err => console.log);
   },
-  activity: (scene, status, title, userId, userName, userType, data) => {
+  activity: async (scene, status, title, userId, userName, userType, data) => {
     axios.post(process.env.URL_SERVICE_LOG+"/v1/activity", {scene, status, title, userId, userName, userType, data}).then(res => console.log).catch(err => console.log);
   }
 }
